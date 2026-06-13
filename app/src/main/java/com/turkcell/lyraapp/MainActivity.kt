e26f4d7d5d2c9645a7bf61e58cf4d1e537c04c70
Emircan Açar <emircanacardev@gmail.com>
2026-06-13 09:25:50 +0300

    first commit

---
+++ app/src/main/java/com/turkcell/lyraapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.turkcell.lyraapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.turkcell.lyraapp.ui.navigation.LyraNavHost
+import com.turkcell.lyraapp.ui.theme.LyraAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LyraAppTheme {
+                LyraNavHost()
+            }
+        }
+    }
+}
